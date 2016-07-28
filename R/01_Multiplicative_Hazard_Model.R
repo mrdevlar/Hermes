1f761ac71f2b8d155c300c33669422cef0264716
Richard Podkolinski <devlar@gmail.com>
2016-07-28 14:58:20 +0100

    censoring tweak

--- R/01_Multiplicative_Hazard_Model.R
+++ R/01_Multiplicative_Hazard_Model.R
@@ -40,8 +40,8 @@ park_var  = c(1, 0.1, 0.5, 0.8, 0.3)
 park_eff  = c(1, 1.1, 1.3, 1.01, 1.2)
 park_Zij  = numeric()
 for(i in seq_along(park_N)){
-    out = rgamma(park_N[i], 1/park_var[i], 1/park_var[i])
-    park_Zij = c(park_Zij, out)
+  out = rgamma(park_N[i], 1/park_var[i], 1/park_var[i])
+  park_Zij = c(park_Zij, out)
 }
 park_Zij = park_Zij * park_eff
 
@@ -51,10 +51,10 @@ summary(park_Zij)
 
 # Model Type
 type_N = c(125, 25, 0,
-            75 , 50, 25,
-            0  , 75, 25,
-            40 ,  0, 10,
-            0  , 30, 20)
+           75 , 50, 25,
+           0  , 75, 25,
+           40 ,  0, 10,
+           0  , 30, 20)
 type_N = type_N * (N / sum(type_N))
 type = rep( rep(1:3,5) , type_N)
 type_eff = rep( rep(c(1.6,1.01,1.3), 5) , type_N)
@@ -102,7 +102,8 @@ lifetime = ((gam^alp) * (-log(runif(N)) / exp(lin_pred)) )^(1/alp)
 
 
 ## Censoring and observed lifetimes
-cens_time = runif(N, min = 0, max = max(0.03 *lifetime) )
+cens_tweak = 0.03
+cens_time = runif(N, min = 0, max = max(cens_tweak *lifetime) )
 d_i = as.numeric(lifetime < cens_time)
 lifetime = apply(cbind(lifetime, cens_time), 1, min)
 sum(d_i)
@@ -128,65 +129,65 @@ stan_data = list(
 m_code = "
 
 functions {
-  real log_h_t(real lifetime, real alp, real gam, real lin_pred);
-  real H_t(real lifetime, real alp, real gam, real lin_pred);
+real log_h_t(real lifetime, real alp, real gam, real lin_pred);
+real H_t(real lifetime, real alp, real gam, real lin_pred);
 
-  real log_h_t(real lifetime, real alp, real gam, real lin_pred){
-    return log( (alp/gam) ) + (alp - 1) * log( (lifetime / gam) ) + lin_pred;
+real log_h_t(real lifetime, real alp, real gam, real lin_pred){
+return log( (alp/gam) ) + (alp - 1) * log( (lifetime / gam) ) + lin_pred;
 }
 
-  real H_t(real lifetime, real alp, real gam, real lin_pred){
-    return exp(lin_pred) * ( (lifetime / gam) )^alp ;
+real H_t(real lifetime, real alp, real gam, real lin_pred){
+return exp(lin_pred) * ( (lifetime / gam) )^alp ;
 }
 
-  real surv_dens_log(vector cens_lifetime, real alp, real gam, real lin_pred){
-    real lifetime;
-    real d_i;
-  
-    lifetime <- cens_lifetime[1];
-    d_i      <- cens_lifetime[2];
-    
-    return d_i * log_h_t(lifetime, alp, gam, lin_pred) - H_t(lifetime, alp, gam, lin_pred);
-  }
+real surv_dens_log(vector cens_lifetime, real alp, real gam, real lin_pred){
+real lifetime;
+real d_i;
+
+lifetime <- cens_lifetime[1];
+d_i      <- cens_lifetime[2];
+
+return d_i * log_h_t(lifetime, alp, gam, lin_pred) - H_t(lifetime, alp, gam, lin_pred);
+}
 }
 
 
 data {
-  int<lower=0>  N;
-  vector[2]     lifetime[N];
-  real          x_err1[N];
-  real          x_err2[N];
-  real          x_repair[N];
-  real          x_kWh[N];
-  real          park_weather[N];
+int<lower=0>  N;
+vector[2]     lifetime[N];
+real          x_err1[N];
+real          x_err2[N];
+real          x_repair[N];
+real          x_kWh[N];
+real          park_weather[N];
 }
 
 
 parameters {
-  real<lower=0> alp;
-  real<lower=0> gam;
-  real          beta_err1;
-  real          beta_err2;
-  real          beta_repair;
-  real          beta_kWh;
-  real          beta_weather;
+real<lower=0> alp;
+real<lower=0> gam;
+real          beta_err1;
+real          beta_err2;
+real          beta_repair;
+real          beta_kWh;
+real          beta_weather;
 }
 
 
 model {
-  for(i in 1:N){
-    lifetime[i] ~ surv_dens(alp, gam, beta_err1 * x_err1[i] + beta_err2 * x_err2[i] + 
-                  beta_repair * x_repair[i] + beta_kWh * x_kWh[i] + beta_weather * park_weather[i]);
-  }
+for(i in 1:N){
+lifetime[i] ~ surv_dens(alp, gam, beta_err1 * x_err1[i] + beta_err2 * x_err2[i] + 
+beta_repair * x_repair[i] + beta_kWh * x_kWh[i] + beta_weather * park_weather[i]);
+}
 
-  alp ~ lognormal(0, 1.5);
-  gam ~ lognormal(0, 1.5);
-  
-  beta_err1 ~ normal(0, 1);
-  beta_err2 ~ normal(0, 10);
-  beta_repair ~ normal(0, 1);
-  beta_kWh ~ normal(0,10);
-  beta_weather ~ normal(0, 1);
+alp ~ lognormal(0, 1.5);
+gam ~ lognormal(0, 1.5);
+
+beta_err1 ~ normal(0, 1);
+beta_err2 ~ normal(0, 10);
+beta_repair ~ normal(0, 1);
+beta_kWh ~ normal(0,10);
+beta_weather ~ normal(0, 1);
 }
 
 "
